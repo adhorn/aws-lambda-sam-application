453ef3420913cad8dd05aa1e0f35c0fe688d34ec
Hornsby <adhorn@3c22fb86c7d0.ant.amazon.com>
2020-09-04 09:27:38 +0300

    clean up

--- pre-traffic-hook/app.py
+++ pre-traffic-hook/app.py
@@ -6,24 +6,26 @@ import os
 def lambda_handler(event, context):
     deploymentId = event["DeploymentId"]
     lifecycleEventHookExecutionId = event["LifecycleEventHookExecutionId"]
-    client = boto3.client("lambda")
-    codeclient = boto3.client("codedeploy")
+    lambda_client = boto3.client("lambda")
     payload = {"message": "ping"}
-    function_name = os.environ["CurrentVersion"]
-    status = "Failed"
-    resp = client.invoke(
+    function_name = os.environ["CurrentVersion"]  # [docs] recommended to pass the lambda version through the Environment Variables
+    resp = lambda_client.invoke(
         FunctionName=function_name,
         InvocationType="RequestResponse",
         Payload=json.dumps(payload),
     )
     dic = json.loads(resp["Payload"].read().decode("utf-8"))
 
+    status = "Failed"
+
     if dic["statusCode"] == 200:
         status = "Succeeded"
     else:
         status = "Failed"
 
-    _ = codeclient.put_lifecycle_event_hook_execution_status(
+    codedeploy_client = boto3.client("codedeploy")
+
+    _ = codedeploy_client.put_lifecycle_event_hook_execution_status(
         deploymentId=deploymentId,
         lifecycleEventHookExecutionId=lifecycleEventHookExecutionId,
         status=status,
