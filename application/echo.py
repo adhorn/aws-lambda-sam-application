249239873ca1dae91a760a683e06bb36a18bc6cc
Adrian Hornsby <adhorn@amazon.com>
2022-02-04 12:35:17 +0200

    adding chaos config env variable to template

--- application/echo.py
+++ application/echo.py
@@ -1,10 +1,7 @@
 import json
-import os
 from aws_lambda_powertools import Logger, Metrics, Tracer
 from aws_lambda_powertools.metrics import MetricUnit
 from chaos_lambda import inject_fault
-
-os.environ['CHAOS_PARAM'] = 'chaoslambda.config'
 
 
 tracer = Tracer(service="echo")
@@ -12,10 +9,10 @@ logger = Logger(service="echo")
 metrics = Metrics(service="echo", namespace="EchoService")
 
 
+@inject_fault
 @metrics.log_metrics(capture_cold_start_metric=True)
 @tracer.capture_lambda_handler
 @logger.inject_lambda_context
-@inject_fault
 def lambda_handler(event, context):
 
     metrics.add_metric(name="EchoSucceeded", value=1, unit=MetricUnit.Count)
